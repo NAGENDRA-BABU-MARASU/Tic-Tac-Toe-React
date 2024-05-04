9b3ac282fe4aadd07054663f870ee81458262bef
Nagendra <nagendrababu.marasu@gmail.com>
2024-05-04 18:23:06 +0530

    replaced repetitive squares code using map

--- src/App.jsx
+++ src/App.jsx
@@ -33,35 +33,36 @@ function Board({ xIsNext, squares, onPlay }) {
 	return (
 		<>
 			<div className="status">{status}</div>
-			<div className="board-row">
-				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-			</div>
-			<div className="board-row">
-				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-			</div>
-			<div className="board-row">
-				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-			</div>
+			{Array(3)
+				.fill(0)
+				.map((element, rowIndex) => {
+					return (
+						<div key={rowIndex} className="board-row">
+							{Array(3)
+								.fill(0)
+								.map((element, colIndex) => {
+									const index = rowIndex * 3 + colIndex;
+									return (
+										<Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />
+									);
+								})}
+						</div>
+					);
+				})}
 		</>
 	);
 }
 
 export default function Game() {
 	const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
+	const [currentMove, setCurrentMove] = useState(0);
+	const xIsNext = currentMove % 2 === 0;
 	const currentSquares = history[currentMove];
 
 	function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
 		setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
+		setCurrentMove(nextHistory.length - 1);
 	}
 
 	function jumpTo(nextMove) {
